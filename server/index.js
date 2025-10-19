1789ad030ac1654212bb748119ea047cacf580d9
zeke614 <ezekielarkohamissah@gmail.com>
2025-10-19 06:13:38 -0800

    fixes

--- server/index.js
+++ server/index.js
@@ -9,20 +9,27 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors());
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: [
+      "https://loop-gh.vercel.app",
+      "http://localhost:5173"       //local testing
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 // Routes
 app.use("/api/auth", authRoutes);
 
-// Connect to MongoDB
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected successfully");
     app.listen(process.env.PORT || 5000, () =>
-      console.log(`Server running on port ${process.env.PORT}`)
+      console.log(`Server running on port ${process.env.PORT || 5000}`)
     );
   })
   .catch((err) => console.error("MongoDB connection error:", err));
